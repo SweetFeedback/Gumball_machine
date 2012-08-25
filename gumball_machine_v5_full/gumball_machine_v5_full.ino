9edf8b4225c14f1f46dd71e953da495bc7059a6f
Jimmy Wang <wang.jimmy319@gmail.com>
2012-08-24 18:03:00 -0700

    change sensorNum to 5, and add detailed sound calibration

--- gumball_machine_v5_full/gumball_machine_v5_full.ino
+++ gumball_machine_v5_full/gumball_machine_v5_full.ino
@@ -13,7 +13,7 @@ const int MotorOutPin = 4; // Motor connected from digital pin 13 to ground
 const int SpeakerOutPin = 8; // Speaker connected from digital pin 8 to ground
 const int LedOutPin = 10; // LED connected from digital pin 12 to ground
 
-const int sensorNum = 6;
+const int sensorNum = 5;
 int sensorValue[sensorNum] = {0};        // initialize value read from the pot
 int outputValue[sensorNum] = {0};        // initialize value output to the PWM (analog out)
 int print_mask[sensorNum] = {0};       // 0 if the data is not going to show, 1 otherwise
@@ -28,19 +28,11 @@ int smoothedDistance = 0;
 int balanceMicVal = -1;
 enum tempUnit{Kelvin,Celcius,Fahrenheit};
 
-//for group collaboration experiment
-int count = 0;
-long total = 0;
-int index = 0;
 const int numReadings = 500;
 int micReadings[numReadings] = {0};
-const double THRES40DB = 36.7/2;
-const double THRES45DB = 58.3/2;
-const double THRES50DB = 70.3/2;
-const double THRES55DB = 96.3/2;
-const double THRES60DB = 139.7/2;
-const double THRES65DB = 216/2;
-const double THRES70DB = 269/2;
+long total = 0;
+int index = 0;
+
 
 void setup() {
   // initialize serial communications at 9600 bps:
@@ -72,7 +64,7 @@ void getSensorData() {
   sensorValue[2] = analogRead(ThermtrInPin);  
   sensorValue[3] = analogRead(DistanceInPin);
   sensorValue[4] = getWindowState();
-  sensorValue[5] = digitalRead(BottomSwitchInPin);
+  //sensorValue[5] = digitalRead(BottomSwitchInPin);
 
   // Sensor calibration
   outputValue[0] = noiseLevel(sensorValue[0]);
@@ -80,7 +72,7 @@ void getSensorData() {
   outputValue[2] = thermistorCalibration(sensorValue[2], Celcius);  
   outputValue[3] = distanceCalibration(sensorValue[3]);
   outputValue[4] = sensorValue[4];
-  outputValue[5] = sensorValue[5];
+  //outputValue[5] = sensorValue[5];
 }
 
 void calcBalanceMicVal(int micVal){
@@ -112,53 +104,15 @@ void serialCallResponse(){
     int inByte = Serial.read();
     int i;
     if (inByte == 'A') {
-      giveCandies();
+        giveCandies();
     }else if (inByte == 'B') {
-      for(i = 0; i < sensorNum -1; i++){
-        Serial.print(outputValue[i]);
-        Serial.print(",");
-      }
-      Serial.println(outputValue[i]);
+        for(i = 0; i < sensorNum -1; i++){
+              Serial.print(outputValue[i]);
+              Serial.print(",");
+        }
+        Serial.println(outputValue[i]);
     }else if (inByte == 'C'){
-      playDisappointedSound();
-    }else if (inByte == 'D'){ //incremental positive feedback
-      count += 1;
-      if(count >= 10){
-        for (int i = 0; i <= 80; i++) {
-          tone(SpeakerOutPin, (400+count*40) * pow(1+i/60.0, 4));
-          delay(5);
-          noTone(SpeakerOutPin);
-        }
-        count = 0;
-      }else{
-        for (int i = 0; i <= 30; i+=1) {
-          tone(SpeakerOutPin, (400+count*40) * pow(1+i/60.0, 2));
-          delay(7);
-          noTone(SpeakerOutPin);
-        }
-      }
-    }
-    
-    //support for customized tone
-    switch(inByte) {
-      case '1': 
-      case '2':
-      case '3':
-      case '4':
-      case '5':
-      case '6':
-      case '7':
-      case '8':
-      case '9':
-        tone(SpeakerOutPin, 400+(inByte-48)*40);
-        delay(80);
-        noTone(SpeakerOutPin);
-      break;
-      case '0':
-        tone(SpeakerOutPin, 400+(10)*40);
-        delay(80);
-        noTone(SpeakerOutPin);
-      break;
+        playDisappointedSound();
     }
   }
 }
@@ -193,26 +147,19 @@ int distanceCalibration(int input){
     return 9462/(input - 16.92);
   return MAX_DISTANCE;
 }
+/* THRES40DB = 36.7;
+   THRES45DB = 58.3;
+   THRES50DB = 70.3;
+   THRES55DB = 96.3;
+   THRES60DB = 139.7;
+   THRES65DB = 216;
+   THRES70DB = 269;*/
 int noiseLevel(int micVal){  
   calcBalanceMicVal(micVal);
   int difference = abs(balanceMicVal - micVal);
-  if(difference < THRES40DB)
-    return 20;
-  else if(difference < THRES45DB)
-    return 40;
-  else if(difference < THRES50DB)
-    return 45;
-  else if(difference < THRES55DB)
-    return 50;
-  else if(difference < THRES60DB)
-    return 55;
-  else if(difference < THRES65DB)
-    return 60;
-  else if(difference < THRES70DB)
-    return 65;
-  else
-    return 70;
-
+  if(difference< 3) 
+  return 0;
+  return 14.9620*(log(difference))-14.2486;
 }
 
 // This is the calibration function for thermistor P/N:NTCLE413E2103H400
@@ -236,6 +183,7 @@ double thermistorCalibration(int RawADC, int Unit) {
    temp = log(RT);
    long double divisor = (A + (B + (C + D * temp)* temp )* temp);
    temp = 1/ divisor;
+   temp += 4;
   if(Unit == Kelvin)
     return temp;
   else if(Unit == Celcius)
